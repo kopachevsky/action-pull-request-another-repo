5be0d808b042184b91675728a45f0021909cda05
akopachevskyy <kopachevsky@gmail.com>
2021-08-17 18:52:39 +0300

    ci/cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,12 +34,8 @@ git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$C
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
-echo "ls dest folder"
-ls "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
-#echo "pwd"
-#echo $(pwd)
-echo "next"
-cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
+
+cp -i $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
 git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
